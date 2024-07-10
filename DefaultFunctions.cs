c28850a8602494951fcc700dd513e403ae8fc5a2
Paceli <fpaceli@gmail.com>
2024-07-10 20:52:04 -0300

    Biblioteca para teste de função externa

---
+++ DefaultFunctions.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibraryConsole
+{
+    public class DefaultFunctions
+    {
+        public double Soma(params double[] args)
+        {
+            double result = 0;
+
+            foreach (var value in args)
+            {
+                result += value;
+            }
+
+            return result;
+        }
+    }
+}
